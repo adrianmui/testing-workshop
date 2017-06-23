e349f664b580aa02e78ba46f83c51d04cef467f8
adrian mui <adrian.mui@gmail.com>
2017-06-23 16:54:19 -0500

    sum.js tdd

--- api/demo/unit/__tests__/sum.js
+++ api/demo/unit/__tests__/sum.js
@@ -1,2 +1,18 @@
 // Let's test drive summing numbers
-test('todo!', () => {})
+
+import sum from './../sum'
+
+test('expects 0 when nothing is passed', () => {
+  const result = sum()
+  expect(result).toBe(0)
+})
+
+test('expects 1 + 2 to be 3', () => {
+  const result = sum(1, 2)
+  expect(result).toBe(3)
+})
+
+test('expects 1 + 2 + 3 to be 6', () => {
+  const result = sum(1, 2, 3)
+  expect(result).toBe(6)
+})
--- api/demo/unit/sum.js
+++ api/demo/unit/sum.js
@@ -1,1 +1,15 @@
 // let's sum some numbers with TDD
+
+export default sum
+
+function sum(inputs) {
+  let val = 0
+  const args = Array.prototype.slice.call(arguments)
+  if (args.length > 0) {
+    args.forEach(el => val += el)
+    return val
+  } else {
+    return 0
+  }
+  
+}
